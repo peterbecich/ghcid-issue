be2d301948e2f15c2ff5784ceda2fd41cea85c9e
Peter Becich <peterbecich@gmail.com>
2024-11-16 18:09:09 -0800

    ghcid works with ghc98

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem = { self', pkgs, ... }: {
+        haskellProjects.default = {
+          basePackages = pkgs.haskell.packages.ghc98;
+
+          # devShell = {
+          #  # Enabled by default
+          #  enable = true;
+          #
+          #  # Programs you want to make available in the shell.
+          #  # Default programs can be disabled by setting to 'null'
+          #  tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
+          #
+          #  hlsCheck.enable = true;
+          # };
+        };
+
+        # haskell-flake doesn't set the default package, but you can do it here.
+        packages.default = self'.packages.example;
+      };
+    };
+}
